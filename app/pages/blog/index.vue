70d888479f066a1ea4f0400c1c0fc5ad1b8e8fff
Xuan Kiet <nguyenxuankiet294@gmail.com>
2026-09-12 22:37:50 +0700

    refactor(routes): prune deprecated blog/products/releases pages and unused components

--- app/pages/blog/index.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-// 301 SEO Permanent Migration to /journal
-await navigateTo('/journal', { redirectCode: 301, replace: true })
-</script>
-
-<template>
-  <div />
-</template>
--- app/pages/products/index.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-// 301 SEO Permanent Migration to /work
-await navigateTo('/work', { redirectCode: 301 })
-</script>
-
-<template>
-  <div />
-</template>
